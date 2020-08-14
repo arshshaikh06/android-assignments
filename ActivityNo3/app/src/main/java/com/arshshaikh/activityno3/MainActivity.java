12f245413ddc7c9875aba6d04142662f719e7691
Arsh Shaikh <arsh291206@gmail.com>
2020-08-14 15:16:48 +0530

    almost done

--- ActivityNo3/app/src/main/java/com/arshshaikh/activityno3/MainActivity.java
+++ ActivityNo3/app/src/main/java/com/arshshaikh/activityno3/MainActivity.java
@@ -3,12 +3,37 @@ package com.arshshaikh.activityno3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivImage;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        radioGroup.clearCheck();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == 0) {
+                    ivImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_background, getApplicationContext().getTheme()));
+                }else{
+                    ivImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_foreground, getApplicationContext().getTheme()));
+                }
+            }
+        });
+    }
+
+    private void init() {
+        ivImage = findViewById(R.id.ivimage);
+        radioGroup = findViewById(R.id.radioGroup);
     }
 }
